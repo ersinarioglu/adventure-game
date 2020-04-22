86d54a2cd85ef5c48aa44a06ab198a40862a014b
Ersin Arioglu <ersin@mit.edu>
2020-04-22 16:21:34 -0400

    Map type definition

--- map.scm
+++ map.scm
@@ -4,3 +4,22 @@
 ;;; Gretchen Eggers, Ersin Arioglu, Nick Janovetz
 
 ;; Map type definition
+
+(define map:places
+  (make-property 'places
+		 'predicate (lambda (x)
+			      (and (n:list? x) (every place?)))
+		 'default-value '()))
+
+
+(define map?
+  (make-type 'map (list map:places)))
+
+(define make-map
+  (type-instantiator map?))
+
+(define get-places
+  (property-getter map:places map?))
+
+(define add-place!
+  (property-adder map:places map? place?)) 
