91899598087c7049579784a3aaaf269fc695337b
Gretchen Eggers <gme@mit.edu>
2020-05-02 22:41:58 -0400

    meeerge

--- package-manager.scm
+++ package-manager.scm
@@ -153,7 +153,7 @@ Start-adventure will create a new environment, load the definitions files into t
                   (display "\nInstallation successful."))
           ((and parent (not child)) (display "\nOops, the package you're trying to install doesn't exist."))
           ((and (not parent) child) (display "\nOops, the point of installation doesn't exist- try listing the packages to see which packages are currently installed."))
-          (else (display "Neither of those packages exist."))
+          (else (display "\nNeither of those packages exist."))
           )))
 
 (define (uninstall-package! package-name) () ) 
@@ -197,8 +197,112 @@ build to symbols in the game environment
 			  packages)
 		     (build (tree:get-root package-tree))
 		     (lowlevel-start-adventure name))))
-        (ge game-env)))
+         (ge game-env))))
 
+(define (get-all-places)
+  all-places)
+
+(define (get-heaven)
+  heaven)
+
+(define (get-clock)
+  the-clock)
+
+;;; Game UI functions
+
+(define (go direction)
+  (let ((exit
+         (find-exit-in-direction direction
+                                 (get-location my-avatar))))
+    (if exit
+        (take-exit! exit my-avatar)
+        (narrate! (list "No exit in" direction "direction")
+                  my-avatar)))
+  'done)
+
+(define (take-thing name)
+  (let ((thing (find-thing name (here))))
+    (if thing
+        (take-thing! thing my-avatar)))
+  'done)
+
+(define (drop-thing name)
+  (let ((thing (find-thing name my-avatar)))
+    (if thing
+        (drop-thing! thing my-avatar)))
+'done)
+
+(define (look-in-bag #!optional person-name)
+  (let ((person
+         (if (default-object? person-name)
+             my-avatar
+             (find-person person-name))))
+    (if person
+        (tell! (let ((referent (local-possessive person))
+                     (things (get-things person)))
+                 (if (n:pair? things)
+                     (cons* referent "bag contains" things)
+                     (list referent "bag is empty")))
+               my-avatar)))
+  'done)
+
+(define (whats-here)
+  (look-around my-avatar)
+  'done)
+
+(define (say . message)
+  (say! my-avatar message)
+  'done)
+
+(define (tell person-name . message)
+  (tell! message (find-person person-name))
+  'done)
+
+(define (hang-out ticks)
+  (do ((i 0 (n:+ i 1)))
+      ((not (n:< i ticks)))
+    (tick! (get-clock)))
+  'done)
+
+;;; Game UI support
+
+(define (here)
+  (get-location my-avatar))
+
+(define (find-person name)
+  (let ((person
+         (find-object-by-name name (people-here my-avatar))))
+    (if (not person)
+        (tell! (list "There is no one called" name "here")
+               my-avatar))
+    person))
+
+(define (find-thing name person-or-place)
+  (let ((thing
+         (find-object-by-name
+          name
+          (person-or-place-things person-or-place))))
+    (if (not thing)
+        (tell! (cons* "There is nothing called"
+                      name
+                      (person-or-place-name person-or-place))
+               my-avatar))
+    thing))
+
+(define (person-or-place-things person-or-place)
+  (if (place? person-or-place)
+      (all-things-in-place person-or-place)
+      (get-things person-or-place)))
+
+(define (person-or-place-name person-or-place)
+  (if (place? person-or-place)
+      '("here")
+      (list "in" (local-possessive person-or-place) "bag")))
+
+(define (local-possessive person)
+  (if (eqv? person my-avatar)
+      "Your"
+      (possessive person)))
 
 ;;; UI ANNOUNCEMENT
 
