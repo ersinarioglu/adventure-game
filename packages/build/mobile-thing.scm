a7c03b1fc07e08f850c7f82679b4d1bceb53afb5
Nick <janonich@AUGUSTINE.mit.edu>
2020-05-04 01:19:44 -0400

    part 1 of build

---
+++ packages/build/mobile-thing.scm
@@ -0,0 +1,5 @@
+(define sicp (build '(mobile-thing sicp)))
+(define engineering-book (build '(mobile-thing engineering-book)))
+(define recitation-problem (build '(mobile-thing recitation-problem)))
+
+(list sicp engineering-book recitation-problem)
