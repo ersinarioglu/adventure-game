66c341cf601a7610f3ab06d21db1d66b43b643fa
Gretchen Eggers <gme@mit.edu>
2020-05-04 12:44:04 -0400

    tweak with medical for demo

--- packages/objects/custom/mit-medical.scm
+++ packages/objects/custom/mit-medical.scm
@@ -1,11 +1,11 @@
 (define mit-medical-visibility
   (create-package 'mit-medical-visibility
                   '()
-                  '()))
+                  'mit-medical))
 (define mit-medical-movement
   (create-package 'mit-medical-movement
                   '()
-                  '()))
+                  'mit-medical))
 (define mit-medical
   (create-package 'mit-medical
                   (list 'mit-medical-visibility 'mit-medical-movement)
