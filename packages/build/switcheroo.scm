cc5564da16c912891c615e9d7ed8f098afc64013
Nick <janonich@AUGUSTINE.mit.edu>
2020-05-04 04:41:10 -0400

    fixed switcheroo-build

--- packages/build/switcheroo.scm
+++ packages/build/switcheroo.scm
@@ -2,6 +2,6 @@
 (define shiny-metallic-ball (build '(switcheroo-button shiny-metallic-ball)))
 
 (set-switcheroo-instance electronic-doohickey shiny-metallic-ball)
-(set-swticheroo-instance shiny-metallic-ball electronic-doohickey)
+(set-switcheroo-instance shiny-metallic-ball electronic-doohickey)
 
 (list electronic-doohickey shiny-metallic-ball)
